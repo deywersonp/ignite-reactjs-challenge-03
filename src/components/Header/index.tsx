4bb06c1c089e88f0f4bcf144aa9f39ccac456ce3
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-08-06 15:44:02 -0300

    added content at Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={commonStyles.container}>
+      <div className={styles.header}>
+        <Link href="/">
+          <a>
+            <img src="/images/Logo.png" alt="Logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
