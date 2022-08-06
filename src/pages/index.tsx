77dc19bf5e5810eeb9f59395c3b671a4cd979448
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-08-06 15:30:28 -0300

    created home content / added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +26,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>spacetraveling.</title>
+      </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.infoContainer}>
+            <div>
+              <FiCalendar />
+              <time>15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.infoContainer}>
+            <div>
+              <FiCalendar />
+              <time>15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.post}>
+          <h1>Como utilizar Hooks</h1>
+          <p>Pensando em sincronização em vez de ciclos de vida.</p>
+          <div className={styles.infoContainer}>
+            <div>
+              <FiCalendar />
+              <time>15 Mar 2021</time>
+            </div>
+            <div>
+              <FiUser />
+              <span>Joseph Oliveira</span>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
